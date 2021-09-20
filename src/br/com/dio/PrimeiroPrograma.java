b061d93bade6e877250e236c75f2bf9eae1c0a74
Marcelo God <marcelogodoi@outlook.com>
2021-09-20 17:25:07 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		/*int a = 3;
+		int b = 4;
+		
+		System.out.println("hello word \n" + (a+b));
+		*/
+		
+		Gato gato = new Gato ();
+		
+		System.out.println(gato);
+	}
+	
+	
+}
